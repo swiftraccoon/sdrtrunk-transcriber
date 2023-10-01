2eba269bae774134c9257070125e551525200481
Swift Raccoon <47705353+swiftraccoon@users.noreply.github.com>
2023-10-01 16:25:35 -0400

    refactor process_recordings.py

--- process_recordings.py
+++ process_recordings.py
@@ -3,7 +3,6 @@ import openai
 import sqlite3
 import xml.etree.ElementTree as ET
 from pydub import AudioSegment
-from functools import lru_cache
 import time
 import json
 import logging
@@ -21,20 +20,23 @@ logger = logging.getLogger()
 
 
 # Configurations
-RECORDINGS_DIR = "/home/USER/SDRTrunk/recordings"
-XML_PATH = "/home/USER/SDRTrunk/playlist/default.xml"
-DATABASE_PATH = "/home/USER/SDRTrunk/recordings.db"
-TEN_SIGN_FILE = "/home/USER/SDRTrunk/Rutherford_Co_NC_TENSIGN.txt"
-CALLSIGNS_PATH = "/home/USER/SDRTrunk/callsigns.db"
-OPENAI_API_KEY = "KEY"
+RECORDINGS_DIR = "/home/YOUR_USER/SDRTrunk/recordings"
+XML_PATH = "/home/YOUR_USER/SDRTrunk/playlist/default.xml"
+DATABASE_PATH = "/home/YOUR_USER/SDRTrunk/recordings.db"
+TEN_SIGN_FILE = "/home/YOUR_USER/SDRTrunk/COUNTY_TENSIGN.txt"
+CALLSIGNS_PATH = "/home/YOUR_USER/SDRTrunk/callsigns.db"
+OPENAI_API_KEY = "YOUR_KEY_HERE"
 
 
 radio_id_names = {
-    1610092: "FCPD Dispatch",
-    1610051: "Sheriff Dispatch",
-    1610077: "EMS Dispatch",
-    2499936: "NCSHP Dispatch",
-    1610078: "RPD Dispatch",
+    "1610092": "FCPD Dispatch",
+    "1610051": "Sheriff Dispatch",
+    "1610077": "EMS Dispatch",
+    "2499936": "NCSHP Dispatch",
+    "1610078": "RPD Dispatch",
+    "1610018": "EMS CAD",
+    "2499937": "NCSHP Dispatch",
+    "1610019": "FD CAD",
 }
 
 
@@ -75,7 +77,6 @@ def load_callsigns():
     return callsigns
 
 
-#
 # Function to load 10-codes from the file
 def load_ten_codes(file_path):
     with open(file_path, "r") as f:
@@ -96,11 +97,23 @@ def update_transcription_to_json(transcription, ten_codes, callsigns, radio_id):
     for code, description in sorted(
         ten_codes.items(), key=lambda x: len(x[0]), reverse=True
     ):
-        # Look for the code with non-digit boundaries
-        pattern = r"(?<!\d)" + re.escape(code) + r"(?!\d)"
+        # Create two patterns for each ten-code: with and without hyphen
+        code_with_hyphen = code
+        code_without_hyphen = code.replace("10-", "10")
+
+        # Construct regex pattern to match both formats
+        pattern = (
+            r"(?<!\d)("
+            + re.escape(code_with_hyphen)
+            + r"|"
+            + re.escape(code_without_hyphen)
+            + r")(?!\d)"
+        )
         if re.search(pattern, transcription):
             extracted_codes[code] = description
-            # transcription = transcription.replace(code, f"{code} [{description}]")
+            transcription = transcription.replace(
+                code_without_hyphen, code_with_hyphen
+            )  # normalize to the hyphenated version
 
     # Detect callsigns in transcription
     for callsign, name in callsigns.items():
@@ -113,15 +126,9 @@ def update_transcription_to_json(transcription, ten_codes, callsigns, radio_id):
     result.update(extracted_codes)  # Merge ten codes into result
     result.update(extracted_callsigns)  # Merge callsigns into result
 
-    # transcribed = json.loads(json.dumps(result))
-    # for key in transcribed:
-    #     transcribed[key] = json.loads(transcribed[key])
-    # encoded = json.dumps(transcribed, indent=4)
-
     return json.dumps(result)
 
 
-@lru_cache(maxsize=None)
 def get_talkgroup_name(xml_path, talkgroup_id):
     if not hasattr(get_talkgroup_name, "talkgroup_dict"):
         # Parse the XML file and create a dictionary of talkgroup IDs and names
@@ -160,7 +167,6 @@ def curl_transcribe_audio(file_path):
         }
         data = {
             "model": "whisper-1",
-            "prompt": "Transcribe the radio dispatch audio. The speaker is usually a dispatcher, police officer, or EMS responder. There are often callsigns, ten-codes, and addresses said.",
             "response_format": "json",
             "temperature": "0",
             "language": "en",
@@ -181,14 +187,9 @@ def extract_radio_id(filename):
         return "Unknown ID"
 
 
-def main():
+def connect_to_database():
     conn = sqlite3.connect(DATABASE_PATH)
     cur = conn.cursor()
-
-    # Load 10-codes
-    ten_codes = load_ten_codes(TEN_SIGN_FILE)
-
-    # Create the table if it doesn't exist
     cur.execute(
         """
     CREATE TABLE IF NOT EXISTS recordings (
@@ -206,88 +207,85 @@ def main():
     )
     """
     )
+    return conn, cur
 
+
+def process_file(file, ten_codes):
+    logger.info(f"Processing file: {file}")
+    if not file.endswith(".mp3"):
+        return
+
+    full_path = os.path.join(RECORDINGS_DIR, file)
+    file_duration = str(get_duration(full_path) / 1000)
+
+    # Check duration and delete if less than 14 seconds
+    if round(float(file_duration)) < 9:
+        os.remove(full_path)
+        return
+
+    # Extract details from filename
+    date, time_part = file.split("_")[:2]
+    time_str = time_part[:2] + ":" + time_part[2:4]
+    unixtime = int(
+        time.mktime(time.strptime(date + " " + time_str, "%Y%m%d %H:%M"))
+    )
+    talkgroup_id = file.split("TO_")[1].split("_")[0]
+    only_radio_id = extract_radio_id(file)
+    radio_id = get_formatted_radio_id(only_radio_id)
+
+    # Move the file based on talkgroup ID
+    new_dir = os.path.join(RECORDINGS_DIR, talkgroup_id)
+    if not os.path.exists(new_dir):
+        os.mkdir(new_dir)
+    new_path = os.path.join(new_dir, file)
+    os.rename(full_path, new_path)
+
+    # Transcribe the audio
+    transcription = curl_transcribe_audio(new_path)
+    logger.info(f"Transcribed text for {file}: {transcription}")
+
+    # Update transcription with 10-codes and callsigns
+    callsign_data = load_callsigns()
+    updated_transcription_json = str(
+        update_transcription_to_json(
+            transcription, ten_codes, callsign_data, radio_id
+        )
+    )
+
+    # Write transcription to a text file
+    try:
+        logger.info(f"Starting to write to text file for {file}")
+        with open(new_path.replace(".mp3", ".txt"), "w") as text_file:
+            text_file.write(updated_transcription_json)
+    except Exception as e:
+        logger.error(f"Error while writing to text file: {str(e)}")
+
+    # Get the talkgroup name from XML
+    talkgroup_name = get_talkgroup_name(XML_PATH, talkgroup_id)
+    return date, time_str, unixtime, talkgroup_id, radio_id, file_duration, file, new_path, transcription, updated_transcription_json, talkgroup_name
+
+
+def insert_into_database(cur, data):
+    try:
+        logger.info(
+            f"Preparing to insert into SQLite for {data[6]}: Date-{data[0]}, Time-{data[1]}, UnixTime-{data[2]}, TalkgroupID-{data[3]}, RadioID-{data[4]}, Duration-{data[5]}, Path-{data[7]}, Transcription-{data[8]}"
+        )
+        cur.execute(
+            """
+            INSERT INTO recordings (date, time, unixtime, talkgroup_id, talkgroup_name, radio_id, duration, filename, filepath, transcription, v2transcription)
+            VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
+            """, data)
+    except Exception as e:
+        logger.error(f"Error while inserting into database: {str(e)}")
+
+
+def main():
+    conn, cur = connect_to_database()
+    ten_codes = load_ten_codes(TEN_SIGN_FILE)
     for file in os.listdir(RECORDINGS_DIR):
-        logger.info(f"Processing file: {file}")
-        if file.endswith(".mp3"):
-            full_path = os.path.join(RECORDINGS_DIR, file)
-
-            file_duration = str(get_duration(full_path) / 1000)
-
-            # Check duration and delete if less than 14 seconds
-            if round(float(file_duration)) < 14:
-                os.remove(full_path)
-                # logger.warning(
-                #     f"File {file} removed due to short duration of {file_duration}"
-                # )
-                continue
-
-            # Extract details from filename
-            date, time_part = file.split("_")[:2]
-            time_str = time_part[:2] + ":" + time_part[2:4]
-            unixtime = int(
-                time.mktime(time.strptime(date + " " + time_str, "%Y%m%d %H:%M"))
-            )
-            talkgroup_id = file.split("TO_")[1].split("_")[0]
-            only_radio_id = extract_radio_id(file)
-            radio_id = get_formatted_radio_id(only_radio_id)
-
-            # Move the file based on talkgroup ID
-            new_dir = os.path.join(RECORDINGS_DIR, talkgroup_id)
-            if not os.path.exists(new_dir):
-                os.mkdir(new_dir)
-            new_path = os.path.join(new_dir, file)
-            os.rename(full_path, new_path)
-
-            # Transcribe the audio
-            transcription = curl_transcribe_audio(new_path)
-            logger.info(f"Transcribed text for {file}: {transcription}")
-
-            # Update transcription with 10-codes and callsigns
-            callsign_data = load_callsigns()
-            updated_transcription_json = str(
-                update_transcription_to_json(
-                    transcription, ten_codes, callsign_data, radio_id
-                )
-            )
-
-            try:
-                logger.info(f"Starting to write to text file for {file}")
-                with open(new_path.replace(".mp3", ".txt"), "w") as text_file:
-                    text_file.write(updated_transcription_json)
-            except Exception as e:
-                logger.error(f"Error while writing to text file: {str(e)}")
-
-            # Get the talkgroup name from XML
-            talkgroup_name = get_talkgroup_name(XML_PATH, talkgroup_id)
-
-            # Insert data into SQLite
-            try:
-                logger.info(
-                    f"Preparing to insert into SQLite for {file}: Date-{date}, Time-{time_str}, UnixTime-{unixtime}, TalkgroupID-{talkgroup_id}, RadioID-{radio_id}, Duration-{file_duration}, Path-{new_path}, Transcription-{transcription}"
-                )
-                cur.execute(
-                    """
-                INSERT INTO recordings (date, time, unixtime, talkgroup_id, talkgroup_name, radio_id, duration, filename, filepath, transcription, v2transcription)
-                VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
-                """,
-                    (
-                        date,
-                        time_str,
-                        unixtime,
-                        talkgroup_id,
-                        talkgroup_name,
-                        radio_id,
-                        file_duration,
-                        file,
-                        new_path,
-                        transcription,
-                        updated_transcription_json,
-                    ),
-                )
-            except Exception as e:
-                logger.error(f"Error while inserting into database: {str(e)}")
-
+        data = process_file(file, ten_codes)
+        if data:
+            insert_into_database(cur, data)
     conn.commit()
     conn.close()
 
